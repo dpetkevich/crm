bb40df280ddbdfe6dfcda12248bbaaca3fb411aa
Daniel Petkevich <daniel.petkevich@gmail.com>
2015-10-07 11:44:46 -0700

    hi

--- run.py
+++ run.py
@@ -6,8 +6,22 @@ app = Flask(__name__)
 
 @app.route("/boristheanimal5423", methods=['GET','POST'])
 def hello():
+	print 'hihi'
+	print "url is"
+	print request.url
+	print "form is "
+	print request.form
+	print 'json is'
 	print request.json
+	print 'value are'
 	print request.values
+	print 'args are'
+	print request.args
+	print "get_json is"
+	print request.get_json
+	print "data is"
+	print request.data
+
 	return "Hello World!"
 
 if __name__ == "__main__":
