0c7c99d9e655e35e8d3c631a8537bea8c9efb3e0
Daniel Petkevich <daniel.petkevich@gmail.com>
2015-10-07 13:37:59 -0700

    hi

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 from flask import Flask, request
 from bs4 import BeautifulSoup
 import requests
+import os
 
 app = Flask(__name__)
+
+host = 'aiaas.pandorabots.com'
+user_key = '8704f84cef67d2c4c1c487ce9aab7da2'
+app_id = '1409612152298'
+botname = os.environ['BOT_NAME']
+crm_username = 'daniel@textbenjamin.com'
+crm_pw = 'boris5423:BEN'
 
 @app.route("/boristheanimal5423", methods=['GET','POST'])
 def hello():
@@ -21,6 +29,36 @@ def hello():
 	print request.get_json
 	print "data is"
 	print request.data
+
+
+	message = request.values.get('message','hi')
+	print 'message'
+	print message
+
+
+	### send message to pandora bots
+	query = "https://aiaas.pandorabots.com/atalk/" + str(app_id) + "/" + str(botname) 
+	
+	payload1 = {
+	'user_key' : str(user_key).encode('utf-8'),
+	"input": str(message).encode('utf-8')
+	}
+
+	pandora_response = requests.post(query, data = payload1)
+
+	full_bot_response = pandora_response.json()
+
+	bot_response = full_bot_response["responses"][0]
+
+	## construct send message request
+
+	url = "https://restapi.crmtext.com/smapi/rest?method=sendsmsmsg"
+
+	payload = {'message':bot_response, 'phone_number': 2812362023}
+
+
+	response = requests.post(url, params = payload, auth=(crm_username,crm_pw))
+
 
 	return "Hello World!"
 
